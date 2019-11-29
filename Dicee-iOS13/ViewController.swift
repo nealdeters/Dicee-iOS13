129b150b7657467824e272f90c7739e9ebe550f0
Neal Deters <nealdeters@gmail.com>
2019-11-28 20:53:57 -0600

    created swift dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // define dice images
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        // set the dice constant array
+        let dice = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+        
+        // set dice 1 and 2 to random numbers
+        diceImageView1.image = dice[Int.random(in: 0...5)]
+        diceImageView2.image = dice[Int.random(in: 0...5)]
+    }
+    
 }
 
